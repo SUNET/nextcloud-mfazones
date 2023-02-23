cc4b85c868da0251e167e67999cdf8a130ce4109
MohammadReza vahedi <34796044+mrvahedi68@users.noreply.github.com>
2023-02-23 04:57:57 +0000

    fix bugs and add idempotency

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 // SPDX-FileCopyrightText: Pondersource <michiel@pondersource.com>
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
@@ -10,6 +11,9 @@ use OCP\SystemTag\ISystemTagManager;
 use OCA\WorkflowEngine\Manager;
 use Psr\Log\LoggerInterface;
 use Doctrine\DBAL\Exception;
+use OCA\WorkflowEngine\Helper\ScopeContext;
+use OCP\WorkflowEngine\IManager;
+use OCP\IDBConnection;
 
 class Application extends App {
 	public const APP_ID = 'mfaverifiedzone';
@@ -24,12 +28,15 @@ class Application extends App {
 	/** @var LoggerInterface */
 	private $logger;
 
+	/** @var IDBConnection */
+	protected $connection;
+
 	public function __construct() {
 		parent::__construct(self::APP_ID);
 
-        if (!\OCP\App::isEnabled('files_accesscontrol')) {
-            throw new Exception("MFA Zone needs files_accesscontrol app to be enabled before installation.");
-        }
+        // if (!\OCP\App::isEnabled('files_accesscontrol')) {
+        //     throw new Exception("MFA Zone needs files_accesscontrol app to be enabled before installation.");
+        // }
 
         $container = $this->getContainer();
         $server = $container->getServer();
@@ -38,6 +45,7 @@ class Application extends App {
         $this->systemTagManager = $this->getContainer()->get(ISystemTagManager::class);
         $this->manager = $this->getContainer()->get(Manager::class);
         $this->logger = $this->getContainer()->get(LoggerInterface::class);
+        $this->connection = $this->getContainer()->get(IDBConnection::class);
 
         $eventDispatcher->addListener('OCA\Files::loadAdditionalScripts', function() {
             \OCP\Util::addStyle(self::APP_ID, 'tabview' );
@@ -48,24 +56,70 @@ class Application extends App {
             \OC::$server->getContentSecurityPolicyManager()->addDefaultPolicy($policy);
         });
 
-        $tags = $this->systemTagManager->getAllTags(
-			null,
-			self::TAG_NAME
-		);
-
-        if(count($tags) < 1){
-            $this->systemTagManager->createTag(self::TAG_NAME, false, false);
-        }
-
+        $this->addTag();
         $this->addFlows();
 	}
 
+    private function addTag(){
+        try{
+            $tags = $this->systemTagManager->getAllTags(
+                null,
+                self::TAG_NAME
+            );
+
+            if(count($tags) < 1){
+                $this->systemTagManager->createTag(self::TAG_NAME, false, false);
+            }
+        }catch (Exception $e) {
+            $this->logger->error('Error when inserting tag on enabling MFAverifiedzone app', ['exception' => $e]);
+        }
+    }
+
     private function addFlows(){
         try {
+            $hash = md5('OCA\WorkflowEngine\Check\MfaVerified::!is::');
+
+            $query = $this->connection->getQueryBuilder();
+            $query->select('id')
+                ->from('flow_checks')
+                ->where($query->expr()->eq('hash', $query->createNamedParameter($hash)));
+            $result = $query->execute();
+
+            if ($row = $result->fetch()) {
+                $result->closeCursor();
+                return;
+            }
+
+            $tags = $this->systemTagManager->getAllTags(
+                null,
+                self::TAG_NAME
+            );
+            $tag = current($tags);
+            $tagId = $tag->getId();
+
             $scope = new ScopeContext(IManager::SCOPE_ADMIN);
             $class = "OCA\\FilesAccessControl\\Operation";
             $name = "";
-            $checks = json_decode('[{"class":"OCA\\WorkflowEngine\\Check\\MfaVerified","operator":"!is","value":"","invalid":false},{"class":"OCA\\WorkflowEngine\\Check\\FileSystemTags","operator":"is","value":1,"invalid":false},{"class":"OCA\\WorkflowEngine\\Check\\UserGroupMembership","operator":"!is","value":"admin","invalid":false}]');
+            $checks =  [
+                [
+                      "class" => "OCA\WorkflowEngine\Check\MfaVerified", 
+                      "operator" => "!is", 
+                      "value" => "", 
+                      "invalid" => false 
+                   ], 
+                [
+                         "class" => "OCA\WorkflowEngine\Check\FileSystemTags", 
+                         "operator" => "is", 
+                         "value" => $tagId, 
+                         "invalid" => false 
+                      ], 
+                [
+                            "class" => "OCA\WorkflowEngine\Check\UserGroupMembership", 
+                            "operator" => "!is", 
+                            "value" => "admin", 
+                            "invalid" => false 
+                         ] 
+                ];
             $operation = "deny";
             $entity = "OCA\\WorkflowEngine\\Entity\\File";
             $events = [];
