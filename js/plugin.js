386222e5ae3c3a38cfc6654648b822573e956484
Michiel de Jong <michiel@pondersource.com>
2023-11-28 13:25:32 +0100

    Hidden actions everywhere

--- js/plugin.js
+++ js/plugin.js
@@ -8,26 +8,35 @@ var mfazoneFileListPlugin = {
       // }
 
       fileList.registerTabView(new OCA.mfazones.MfaZoneTabView());
+
       fileList.fileActions.registerAction({
         name: 'mfa',
-        displayName: 'MFA Zone',
+        displayName: '',
         type: 1,
         mime: 'all',
         permissions: OC.PERMISSION_NONE,
-        iconClass: 'icon-category-security',
+        // iconClass: 'icon-category-security',
         actionHandler: function(fileName, context) {
           if (confirm('You must enable two factor authentication to use MFAZone app. Do you want to enable 2FA?')) {
             window.location.href = OC.generateUrl('/settings/user/security');
           }
         },
-        filename: 'asdf'
+        // fileName: 'asdf',
       });
-      const originalSetFiles = fileList.setFiles;
 
+
+      const originalSetFiles = fileList.setFiles;
       fileList.setFiles = (
         function(filesArray) {
           // You can find the setFiles function here: https://github.com/nextcloud/server/blob/master/apps/files/js/filelist.js
           console.log('FILES ARRAY 2>>>>>>', filesArray)
+          filesArray.forEach((file) => {
+            console.log('seeing', file.name);
+            if (file.name === 'asdf') {
+              console.log('registering');
+              }
+            });
+    
           originalSetFiles.bind(fileList)(filesArray)
         }
       ).bind(fileList)
