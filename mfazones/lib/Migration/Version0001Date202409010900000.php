2a38ff24c29366489b87ad93944d79463961687c
Micke Nordin <kano@sunet.se>
2024-09-02 09:55:59 +0200

    Fix copyright headers on source files

--- mfazones/lib/Migration/Version0001Date202409010900000.php
+++ mfazones/lib/Migration/Version0001Date202409010900000.php
@@ -7,7 +7,7 @@ declare(strict_types=1);
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-namespace OCA\mfazone\Migration;
+namespace OCA\mfazones\Migration;
 
 use OCP\Migration\SimpleMigrationStep;
 use OCP\IDBConnection;
